dffc61b447d3632cbe48a7bfba4b89c9e8b9087d
“Kondo <cloud.kenji.kondo@gmail.com>
2025-08-30 20:20:30 +0900

    メンション及び作業関連の項目を通知する機能を追加8

--- lib/redmine_messenger/patches/time_entry_patch.rb
+++ lib/redmine_messenger/patches/time_entry_patch.rb
@@ -33,43 +33,48 @@ module RedmineMessenger
             attachment = {}
             attachment[:fields] = []
 
-            # Add hours
-            attachment[:fields] << { title: I18n.t(:field_hours),
-                                     value: Messenger.markup_format(hours.to_s),
+            # Add hours in h:mm format
+            hours_formatted = format_hours_hm(hours)
+            attachment[:fields] << { title: "作業時間",
+                                     value: Messenger.markup_format(hours_formatted),
                                      short: true }
 
             # Add activity (work category)
             if activity.present?
-              attachment[:fields] << { title: I18n.t(:field_activity),
+              attachment[:fields] << { title: "作業分類",
                                        value: Messenger.markup_format(activity.to_s),
                                        short: true }
             end
 
+            # Add spent date
+            attachment[:fields] << { title: "日付",
+                                     value: Messenger.markup_format(spent_on.to_s),
+                                     short: true }
+
             # Add comments
             if comments.present?
-              attachment[:fields] << { title: I18n.t(:field_comments),
+              attachment[:fields] << { title: "コメント",
                                        value: Messenger.markup_format(comments),
                                        short: false }
             end
 
-            # Add spent date
-            attachment[:fields] << { title: I18n.t(:field_spent_on),
-                                     value: Messenger.markup_format(spent_on.to_s),
-                                     short: true }
+            main_message = if issue.present?
+                            l(:label_messenger_time_entry_created_with_issue,
+                              project_name: Messenger.markup_format(project.name),
+                              tracker: issue.tracker.name,
+                              issue_url: "<#{Messenger.object_url issue}|#{Messenger.markup_format issue.subject}>",
+                              user: Messenger.markup_format(user.to_s))
+                          else
+                            l(:label_messenger_time_entry_created,
+                              project_name: Messenger.markup_format(project.name),
+                              user: Messenger.markup_format(user.to_s))
+                          end
+            
+            # Add mentions on separate lines
+            mentions = build_time_entry_mentions_message
+            full_message = "#{main_message}#{mentions}"
 
-            message = if issue.present?
-                        l(:label_messenger_time_entry_created_with_issue,
-                          project_url: Messenger.project_url_markdown(project),
-                          issue_url: "<#{Messenger.object_url issue}|#{Messenger.markup_format issue}>",
-                          user: user)
-                      else
-                        l(:label_messenger_time_entry_created,
-                          project_url: Messenger.project_url_markdown(project),
-                          user: user)
-                      end
-
-            Messenger.speak send_messenger_time_entry_mention_message(message),
-                            channels, url, attachment: attachment, project: project
+            Messenger.speak full_message, channels, url, attachment: attachment, project: project
           ensure
             ::I18n.locale = initial_language
           end
@@ -95,49 +100,64 @@ module RedmineMessenger
             attachment = {}
             attachment[:fields] = []
 
-            # Add hours
-            attachment[:fields] << { title: I18n.t(:field_hours),
-                                     value: Messenger.markup_format(hours.to_s),
+            # Add hours in h:mm format
+            hours_formatted = format_hours_hm(hours)
+            attachment[:fields] << { title: "作業時間",
+                                     value: Messenger.markup_format(hours_formatted),
                                      short: true }
 
             # Add activity (work category)
             if activity.present?
-              attachment[:fields] << { title: I18n.t(:field_activity),
+              attachment[:fields] << { title: "作業分類",
                                        value: Messenger.markup_format(activity.to_s),
                                        short: true }
             end
 
+            # Add spent date
+            attachment[:fields] << { title: "日付",
+                                     value: Messenger.markup_format(spent_on.to_s),
+                                     short: true }
+
             # Add comments
             if comments.present?
-              attachment[:fields] << { title: I18n.t(:field_comments),
+              attachment[:fields] << { title: "コメント",
                                        value: Messenger.markup_format(comments),
                                        short: false }
             end
 
-            # Add spent date
-            attachment[:fields] << { title: I18n.t(:field_spent_on),
-                                     value: Messenger.markup_format(spent_on.to_s),
-                                     short: true }
+            main_message = if issue.present?
+                            l(:label_messenger_time_entry_updated_with_issue,
+                              project_name: Messenger.markup_format(project.name),
+                              tracker: issue.tracker.name,
+                              issue_url: "<#{Messenger.object_url issue}|#{Messenger.markup_format issue.subject}>",
+                              user: Messenger.markup_format(user.to_s))
+                          else
+                            l(:label_messenger_time_entry_updated,
+                              project_name: Messenger.markup_format(project.name),
+                              user: Messenger.markup_format(user.to_s))
+                          end
+            
+            # Add mentions on separate lines
+            mentions = build_time_entry_mentions_message
+            full_message = "#{main_message}#{mentions}"
 
-            message = if issue.present?
-                        l(:label_messenger_time_entry_updated_with_issue,
-                          project_url: Messenger.project_url_markdown(project),
-                          issue_url: "<#{Messenger.object_url issue}|#{Messenger.markup_format issue}>",
-                          user: user)
-                      else
-                        l(:label_messenger_time_entry_updated,
-                          project_url: Messenger.project_url_markdown(project),
-                          user: user)
-                      end
-
-            Messenger.speak send_messenger_time_entry_mention_message(message),
-                            channels, url, attachment: attachment, project: project
+            Messenger.speak full_message, channels, url, attachment: attachment, project: project
           ensure
             ::I18n.locale = initial_language
           end
         end
 
         private
+
+        def format_hours_hm(hours_decimal)
+          return "0:00" if hours_decimal.blank? || hours_decimal == 0
+          
+          total_minutes = (hours_decimal * 60).round
+          hours_part = total_minutes / 60
+          minutes_part = total_minutes % 60
+          
+          "#{hours_part}:#{minutes_part.to_s.rjust(2, '0')}"
+        end
 
         def messenger_time_entry_to_be_notified
           to_be_notified = []
@@ -151,13 +171,26 @@ module RedmineMessenger
           to_be_notified.compact.uniq
         end
 
-        def send_messenger_time_entry_mention_message(base_message)
-          mention_to = ''
-          if Messenger.setting_for_project(project, :auto_mentions) ||
-             Messenger.textfield_for_project(project, :default_mentions).present?
-            mention_to = Messenger.mentions project, comments
+        def build_time_entry_mentions_message
+          return '' unless issue.present?
+          
+          mentions = []
+          
+          # Add assignee mention
+          if issue.assigned_to.present?
+            assignee_mention = Messenger.format_user_mention(issue.assigned_to)
+            mentions << "\n\n👤 担当者: #{assignee_mention}" if assignee_mention.present?
           end
-          "#{base_message}#{mention_to}"
+          
+          # Add watcher mentions
+          if issue.watcher_users.any?
+            watcher_mentions = issue.watcher_users.map { |user| Messenger.format_user_mention(user) }.compact
+            if watcher_mentions.any?
+              mentions << "\n👁️ ウォッチャー: #{watcher_mentions.join(' ')}\n\n"
+            end
+          end
+          
+          mentions.join
         end
       end
     end
