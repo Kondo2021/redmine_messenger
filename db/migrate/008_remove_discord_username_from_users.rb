8d7e946b35545857ed827350da12e9fa0209440e
“Kondo <cloud.kenji.kondo@gmail.com>
2025-08-30 20:09:23 +0900

    メンション及び作業関連の項目を通知する機能を追加7

--- db/migrate/008_remove_discord_username_from_users.rb
+++ db/migrate/008_remove_discord_username_from_users.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class RemoveDiscordUsernameFromUsers < ActiveRecord::Migration[4.2]
-  def change
-    remove_column :users, :discord_username, :string
+  def up
+    remove_column :users, :discord_username if column_exists?(:users, :discord_username)
+  end
+
+  def down
+    add_column :users, :discord_username, :string unless column_exists?(:users, :discord_username)
   end
 end
